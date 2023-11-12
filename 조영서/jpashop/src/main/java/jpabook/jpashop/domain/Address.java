c037b72d8ca0443dead549172df663ceb4b27717
youngseo cho <dudtj0412@naver.com>
2023-11-12 17:31:30 +0900

    Feat: 5주차 실습코드

--- 조영서/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ 조영서/jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,13 +5,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
 
+<<<<<<< Updated upstream
+=======
+    protected Address() {
+    }
+
+>>>>>>> Stashed changes
     public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
