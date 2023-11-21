456d943a37c759c20f25051e49990e723b39451c
영서 <dudrhy12@sookmyung.ac.kr>
2023-11-22 04:11:00 +0900

    Feat: 6주차 실습코드
    ~ JPA 활용2 의 Section1

--- 조영서/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ 조영서/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,11 +18,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") //주인 지정, 맵핑 된거다, 읽기 전용이 된거다
     private List<Order> orders = new ArrayList<>();
 
